4c3851be361a981104a5d5d21c86cafe3045422c
JuhaMikkola <juha@Juhas-MacBook-Pro.local>
2013-10-22 13:43:31 -0400

    Exercise 2 is Here

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+mealprice = 55 #Meal price
+tiprate = 1.2 #Good tip is 20%
+puts (mealprice * tiprate)
+
+str = "DucksAreInTown"
+int = 6
+puts (str + int.to_s) #ConvertIntegertoString
+
+x = 45628
+y = 7839
+z = x * y
+puts "The answer you are looking for is #{z}. Boom!"
+
+#The answer is True
+puts (true && false) || (false && true) || !(false && false)
