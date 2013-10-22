383578629191baddf8fbc4c33c4faac6e8ad634f
JuhaMikkola <juha@Juhas-MacBook-Pro.local>
2013-10-22 14:17:31 -0400

    Exercise 3 is complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,19 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "Have you already had your birthday this year? Answer YES or NO"
+thisyear = gets.chomp.upcase
+
+if thisyear == "YES"
+	YOB = 2013 - age
+	puts "You were born in #{YOB}"
+elsif thisyear == "NO"
+	YOB = 2012 - age
+	puts "You were born in #{YOB}"
+else
+	puts "Error - please try again and answer YES or NO"
+end
