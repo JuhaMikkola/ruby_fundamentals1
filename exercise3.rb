2a1d27867f383b8530390e344631e8d26090f9f4
JuhaMikkola <juha@Juhas-MacBook-Pro.local>
2013-10-22 15:15:42 -0400

    Voila - Exercise 4 Kind Sirs

--- exercise3.rb
+++ exercise3.rb
@@ -9,10 +9,10 @@ puts "Have you already had your birthday this year? Answer YES or NO"
 thisyear = gets.chomp.upcase
 
 if thisyear == "YES"
-	YOB = 2013 - age
+	YOB = Time.now.year - age
 	puts "You were born in #{YOB}"
 elsif thisyear == "NO"
-	YOB = 2012 - age
+	YOB = Time.now.year - age
 	puts "You were born in #{YOB}"
 else
 	puts "Error - please try again and answer YES or NO"
---
+++ exercise4.rb
@@ -0,0 +1,13 @@
+101.times #Set up the loop
+
+for i in 1..100 do 
+  if ((i % 3 == 0) && (i % 5 == 0)) #Multiples of 3 and 5
+    puts "BitMaker"
+  elsif (i % 3 == 0) #Multiples of 3
+    puts "Bit"
+  elsif (i % 5 == 0) #Multiples of 5
+    puts "Maker"
+  else #All the others
+    puts i  
+  end
+end
